75883e262651c710a5d5ca4da1493e39ed9d7118
Pistachu <ygomezma@gmail.com>
2020-12-17 17:14:44 +0100

    Rediseño de rutas

--- routes/users.js
+++ routes/users.js
@@ -2,79 +2,82 @@ const express = require('express');
 const router = express.Router();
 require('../db/mongoose');
 const User = require('../models/user');
-const Address = require('../models/address');
 
 /* Get all users */
-router.get('/getusers', (req, res) => {
-  User.find({})
-    .then((users) => {
-      res.send({ description: "OK", users });
-    })
-    .catch((e) => {
-      res.status(500).send();
-    })
+router.get('/getusers', async (req, res) => {
+
+  try {
+    const users = await User.find({});
+    res.send({ description: "OK", users });
+  } catch (e) {
+    res.status(500).send();
+  }
+
 });
 
 /* Create user */
-router.post('/createUsers', (req, res) => {
+router.post('/createUsers', async (req, res) => {
   const user = new User(req.body);
-  user.save()
-    .then(() => {
-      res.status(201).send({ description: "CREATED", user });
-    })
-    .catch((e) => {
-      res.status(405).send({ description: "Invalid input" });
-    })
+
+  try {
+    const result = await user.save();
+    res.status(201).send({ description: "CREATED", result });
+  } catch (e) {
+    res.status(405).send({ description: "Invalid input" });
+  }
+
 });
 
 /* Get one user */
-router.get('/getusersById/:userId', (req, res) => {
+router.get('/getusersById/:userId', async (req, res) => {
   const id = req.params.userId;
-  User.findOne({ id: id })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ description: "User not found" });
-      } else {
-        res.send({ description: "OK", user });
-      }
-    })
-    .catch((e) => {
-      res.status(400).send({ description: "Invalid user id" });
-    })
+
+  try {
+    const user = await User.findOne({ id });
+    if (!user) {
+      res.status(404).send({ description: "User not found" });
+    } else {
+      res.send({ description: "OK", user });
+    }
+  } catch (e) {
+    res.status(400).send({ description: "Invalid user id" });
+  }
+
 });
 
 /* Update one user */
-router.put('/updateUsersById/:userId', (req, res) => {
+router.put('/updateUsersById/:userId', async (req, res) => {
   const id = req.params.userId;
   const userDataToUpdate = req.body;
 
-  User.findOneAndUpdate({ id: id }, userDataToUpdate, { new: true })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ description: "User not found" });
-      } else {
-        res.send({ description: "OK", user });
-      }
-    })
-    .catch((e) => {
-      res.status(400).send({ description: "Invalid user id" });
-    })
+  try {
+    const user = await User.findOneAndUpdate({ id }, userDataToUpdate, { new: true, runValidators: true });
+    if (!user) {
+      res.status(404).send({ description: "User not found" });
+    } else {
+      res.send({ description: "OK", user });
+    }
+  } catch (e) {
+    res.status(400).send({ description: "Invalid user id" });
+  }
+
 });
 
 /* Delete one user */
-router.delete('/deleteUsersById/:userId', (req, res) => {
+router.delete('/deleteUsersById/:userId', async (req, res) => {
   const id = req.params.userId;
-  User.findOneAndDelete({ id: id })
-    .then((user) => {
-      if (!user) {
-        res.status(404).send({ description: "User not found" });
-      } else {
-        res.send({ description: "OK", user });
-      }
-    })
-    .catch((e) => {
-      res.status(400).send({ description: "Invalid user id" });
-    })
+
+  try {
+    const user = await User.findOneAndDelete({ id });
+    if (!user) {
+      res.status(404).send({ description: "User not found" });
+    } else {
+      res.send({ description: "OK", user });
+    }
+  } catch (e) {
+    res.status(400).send({ description: "Invalid user id" });
+  }
+
 });
 
 module.exports = router;
